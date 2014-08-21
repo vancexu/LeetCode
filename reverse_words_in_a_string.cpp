431b16322679a6491d61f977f052c93d13844972
vancexu <xuboweinju@gmail.com>
2014-08-21 17:37:38 -0400

    simplify reverse_words_in_a_string.cpp

--- reverse_words_in_a_string.cpp
+++ reverse_words_in_a_string.cpp
@@ -13,10 +13,10 @@ public:
         string res;
         vector<string> words = split(s);
         for (int i = words.size()-1; i >= 0; --i) {
-            res = res + words[i] + " ";
+            res += words[i];
+            if (i != 0)
+                res += " ";
         }
-        if (!res.empty())
-            res.erase(res.size()-1,1);
         s = res;
     }
 
@@ -28,16 +28,21 @@ private:
         for (int i=0; i < s.size(); ++i)
             if (s[i] == ' ')
                 blankIndexes.push_back(i);
-        if (!blankIndexes.empty() && blankIndexes[0] != 0) 
-            words.push_back(s.substr(0,blankIndexes[0]));
-        for (int i=0; i < blankIndexes.size()-1; ++i) {
-            int cur = blankIndexes[i];
-            int next = blankIndexes[i+1];
-            if (next - cur > 1) 
-                words.push_back(s.substr(cur+1, next-cur-1));
+
+        if (blankIndexes.empty()) {
+            words.push_back(s);
+        } else {
+            if (blankIndexes[0] != 0) 
+                words.push_back(s.substr(0,blankIndexes[0]));
+            for (int i=0; i < blankIndexes.size()-1; ++i) {
+                int cur = blankIndexes[i];
+                int next = blankIndexes[i+1];
+                if (next - cur > 1) 
+                    words.push_back(s.substr(cur+1, next-cur-1));
+            }
+            if (blankIndexes.back() != s.size()-1)
+                words.push_back(s.substr(blankIndexes.back()+1));
         }
-        if (!blankIndexes.empty() && blankIndexes.back() != s.size()-1)
-            words.push_back(s.substr(blankIndexes.back()+1));
         return words;
     }
 };
@@ -47,11 +52,14 @@ int main() {
     string s1 = "the sky is blue";
     string s2 = "  the sky  is blue  ";
     string s3 = " ";
+    string s4 = "a";
     sol.reverseWords(s1);
     sol.reverseWords(s2);
     sol.reverseWords(s3);
+    sol.reverseWords(s4);
     cout << s1 << endl;
     cout << s2 << endl;
     cout << s3 << endl;
+    cout << s4 << endl;
     cout << endl;
 }
