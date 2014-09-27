ceb186622b34c65048d8033f262e878f6e411531
vancexu <xuboweinju@gmail.com>
2014-09-27 10:22:41 -0400

    add search_a_2d_matrix.cpp

---
+++ search_a_2d_matrix.cpp
@@ -0,0 +1,51 @@
+/*
+ * Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+ *
+ * Integers in each row are sorted from left to right.
+ * The first integer of each row is greater than the last integer of the previous row.
+ * For example,
+ *
+ * Consider the following matrix:
+ *
+ * [
+ *   [1,   3,  5,  7],
+ *   [10, 11, 16, 20],
+ *   [23, 30, 34, 50]
+ * ]
+ * Given target = 3, return true.
+ */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int> > &matrix, int target) {
+        int rows = matrix.size();
+        if (rows == 0) return false;
+        int cols = matrix[0].size();
+        if (cols == 0) return false;
+
+        int row = 0;
+        for (; row < rows; ++row) {
+            if (matrix[row][cols-1] >= target)
+                break;
+        }
+
+        if (row == rows)
+            return false;
+        return binary_search(matrix[row].begin(), matrix[row].end(), target);
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<vector<int>> mat = {{1,3,5,7},
+                               {10,11,16,20},
+                               {23,30,34,50}};
+    cout << (true == sol.searchMatrix(mat, 11));
+    cout << endl;
+}
